207d4ab9a3ca9e0f5fd824f4649be272a391000a
Puja <pujayadav@gmail.com>
2020-09-27 10:30:30 +0530

    linecomparison1

---
+++ Linecomparison1/Linecomparison1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Linecomparison1
+{
+    class Line
+    {
+
+        public int X1 { get; set; }
+        public int X2 { get; set; }
+        public int Y1 { get; set; }
+        public int Y2 { get; set; }
+        public double Length()
+        {
+            return Math.Sqrt((X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1));
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line1 = new Line { X1 = 0, Y1 = 1, X2 = 0, Y2 = 1 };
+            var line2 = new Line { X1 = 0, X2 = 0, Y1 = -1, Y2 = -1 };
+            Console.WriteLine(line1.Length().Equals(line2.Length()) ? "Lines are Equal" : (line1.Length() > line2.Length() ? "Line 1 is Greater than Line 2" : "Line 2 is Greater than Line1"));
+
+
+        }
+
+
+    }
+}
